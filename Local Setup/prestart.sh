4a68886f1987490d82aecc42e3008a231542e858
Hussain Ravat <hussain.ravat786@gmail.com>
2025-02-26 15:29:11 +1300

    Start of DBT integration with kestra

--- Local Setup/prestart.sh
+++ Local Setup/prestart.sh
@@ -4,6 +4,8 @@
 echo "Starting pre-start script..."
 
 # Move files from ./kestra/python to /home/hussain/kestra-data/dev
-cp -r ./kestra/python/* /home/hussain/kestra-data/dev/_files
+cp -r ./kestra/python/* /home/hussain/kestra-data/api_flows/_files
+cp -r ./dbt_project/* /home/hussain/kestra-data/dbt_project/_files
+cp -r ./dbt/* /home/hussain/kestra-data/dbt_project/_files
 
-echo "Pre-start script completed. Files have been moved successfully."
+echo "Pre-start script completed. Files have been moved successfully."S
